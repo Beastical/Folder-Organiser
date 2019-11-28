25615652e04f2daa23f2c9eba45c038dc1e7ba9e
Anoop George <ganoop24lal@gmail.com>
2019-11-28 12:59:51 +0530

    Completed moveFiles() and minor changes in mapping approach

--- FolderOrganizer.py
+++ FolderOrganizer.py
@@ -11,13 +11,16 @@ master =[]
 
 class FolderOrganizer:
 
+
     def __init__(self):
         # define the Folder -> file type lists
         map,master = self.getMapping()
+        print('init',master)
+        filters =  self.getFilters(map)
         # Set default folder
         os.chdir(path)
         # Traverse source path
-        self.findFiles()
+        self.findFiles(filters)
 
     def createFolder(self,dict):
         # Create folders if they do not exists
@@ -29,6 +32,24 @@ class FolderOrganizer:
                     print("Folder created",fol)
                 except OSError:
                     print("Error occured during folder creation",fol)
+
+    def getFilters(selef,map):
+        print('Generating filters')
+        fltrs = {}
+        # Get the filter list and folders
+        for folder_name in map:
+            values = map[folder_name]
+            values = values.values()
+            folder_name = os.path.join(path,folder_name)
+            print(folder_name)
+            fltrs[folder_name] = values
+        print('Filters ->',fltrs)
+        return fltrs
+
+    def moveFiles(self,src,trg):
+        dest = shutil.move(src, trg)
+        print('Moved to ->',dest)
+
 
     def getMapping(self):
         # Get the mappings from the csv file
@@ -46,19 +67,24 @@ class FolderOrganizer:
         print('Master ->',mstr)
         return df,mstr
 
-    # def moveFile(self,src_f,trg_f):
-    #     dest = shutil.move()
-    #
 
-    def findFiles(self):
+
+    def findFiles(self,fltrs):
+        print('Find files')
         # Traverse and check for files
         for dir,subdir,file in os.walk(src_path):
-            # Go through files in the list
             for f in file:
-                # Check if they match master extension list
-                if(f.endswith('.mkv')):
-                    print(f,'extension')
+                for folders in fltrs:
+                    print(folders,'fltrs',fltrs[folders])
+                    print(f)
+                    key_list = fltrs[folders]
+                    print([ele for ele in key_list if (str(ele) in f)])
+                    if([ele for ele in key_list if (str(ele) in f)]):
+                        src_file_path = os.path.join(dir,f)
+                        trg_file_path = os.path.join(path,folders)
+                        self.moveFiles(src_file_path,trg_file_path)
 
+# print([ele for ele in m if(ele in f)])
 
 if __name__ == '__main__':
     org = FolderOrganizer()
